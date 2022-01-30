6c00702afbbd6643e17b0f16ebadabbe6fef46a2
Shivanshu97i <singhshivanshu97@gmail.com>
2022-01-31 01:09:08 +0530

    Added a api and hooks

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,21 +1,43 @@
+import {useState,useEffect} from "react";
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react/cjs/react.production.min";
 import "./SearchBar.css"
 let SearchBar = () =>
 {
+  const [anime, searchAnime] = useState("");
+  useEffect(()=>{
+    requestAnime();
+  },[]); // eslint-disable-line react-hooks/exhaustive-deps
+
+async function requestAnime(){
+  let url = 'https://api.jikan.moe/v4/anime?q='+ anime; 
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log("error", error);
+    }
+  
+}
+  
+
     return(
         <div className="container form">
           <div className="d-flex justify-content-center">
-               {/* <form className="d-flex w-75 p-3">
-         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" /> 
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-      <div class="wrap">
+              
+      <div className="wrap">
    <div className="search">
-   {/* <form className="d-flex w-75 p-3"> */}
-      <input type="text" className="searchTerm form-control me-2" placeholder="What are you looking for?" />
+   
+      <input type="text" 
+      id="anime"
+      value={anime}
+      onChange={(e)=>searchAnime(e.target.value)}
+      className="searchTerm form-control me-2" 
+      placeholder="What are you looking for?" />
       <button type="submit" className="searchButton">
         <i className="fa fa-search"></i>
      </button>
-     {/* </form> */}
+     
    </div>
 </div>
           </div>
