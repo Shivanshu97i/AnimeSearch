8da7c4b93ae87084f792a6c35a877c1b0ab02b27
Shivanshu97i <singhshivanshu97@gmail.com>
2022-01-31 03:10:47 +0530

    Added Route and AnimeCard.js for displaying search information

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import{ render } from "react-dom";
 import Navbar from "./navbar.js";
 import SearchBar from "./SearchBar.js";
 import Footer from "./footer.js";
-// import Background from "./Background.js";
+
+import Cards from "./AnimeCards.js"
+
 const App = () =>{ 
     return(
         <div>
             
-            <div><Navbar/></div>
+           <Switch>
+               <Route path="/">
+                   <div><Navbar/></div>
         <div><SearchBar/></div>
-        {/* <div><Footer/></div> */}
-        {/* <div><Background/></div> */}
+        <div><Footer/></div>
+        </Route>
+                <Route exact path="/cards"><Cards/></Route> 
+                
+            </Switch>
+        
+        
         </div>
         
         
--- src/footer.js
+++ src/footer.js
@@ -3,14 +3,13 @@ import "./Footer.css"
 let Footer = () =>{
     return(
         <div className="container-fluid footer">
-        <footer className="tag-footer d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <div className="d-flex flex-row">
-            <a href="https://github.com/Shivanshu97i/Aniime-Search-Site" className="icon p-2" ><i class="fab fa-github-alt"></i></a>
-            <a href="https://www.linkedin.com/in/shivanshu-singh-4454311b8/" className="icon p-2"><i class="fab fa-linkedin"></i></a>
-            <a href="" className="icon p-2"><i class="fab fa-discord"></i></a>
+        <footer className="tag-footer d-flex align-items-center py-2 my-4">
+            <div className="d-flex flex-row footer-div">
+            <a href="https://github.com/Shivanshu97i/Aniime-Search-Site" className="icon p-2" ><i className="fab fa-github-alt"></i></a>
+            <a href="https://www.linkedin.com/in/shivanshu-singh-4454311b8/" className="icon p-2"><i className="fab fa-linkedin"></i></a>
+            <a href="" className="icon p-2"><i className="fab fa-discord"></i></a>
             </div>
  
- <p><a href="mailto:singhshivanshu97@gmail.com"><b>Contact Through Mail</b></a></p>
 </footer>
       </div>
   
