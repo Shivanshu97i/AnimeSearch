264c88b9abd9458ae77528fd20d317820833b8bf
Shivanshu97i <singhshivanshu97@gmail.com>
2022-02-12 00:14:20 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import "./styles.css"
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
