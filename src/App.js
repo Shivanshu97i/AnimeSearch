76af7cea0afa53a93b02b0972dc03de2a2840fec
Shivanshu97i <singhshivanshu97@gmail.com>
2022-02-11 23:47:30 +0530

    Removed build tools

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import reactDom from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { render } from "react-dom";
@@ -20,7 +20,23 @@ const App = () => {
         setState(data.data)
 
     };
-    return (
+    return (<>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&family=Ubuntu:wght@300&display=swap" rel="stylesheet"/>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"/>
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"/>
+       {/* <link rel="stylesheet" href="styles.css"/> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+       <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&display=swap" rel="stylesheet"/>
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+       <title>Anime Search</title>
+    </head>
         <div>
 
             
@@ -37,9 +53,8 @@ const App = () => {
 
 
 
-        </div >
+        </div ></>
 
     );
 };
-render(<App />,
-    document.getElementById("root"));
+export default App;
